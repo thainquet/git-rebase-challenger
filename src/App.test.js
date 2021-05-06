3dea6783a2a111310d353acf939cc6b9abbc1999
Alex Hughes <alexhughes@AlexMBPro.local>
2021-05-06 14:09:10 -0500

    feat: add Biology Today article

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders My really cool blog site logo', () => {
+test("renders My really cool blog site logo", () => {
   render(<App />);
   const linkElement = screen.getByText(/My really cool blog site/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders Auto Review Article', () => {
+test("renders Biology Today article", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Auto Review:/i);
+  const linkElement = screen.getByText(/Biology Today:/i);
   expect(linkElement).toBeInTheDocument();
 });
